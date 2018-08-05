f960ad554541a156f2849b7c57ebbd08aae7cb47
Baba Bakchod <bababakchod@protonmail.com>
2018-08-05 16:57:57 +0530

    http client for tpb

--- piratescrape/http_client.py
+++ piratescrape/http_client.py
@@ -1,13 +1,20 @@
 import requests
 
 class HTTPClient:
-	def __init__(self):
-		self.session = requests.Session()
-	
-	def get_tpb(self, page):
-		self.session.get("https://thepiratebay.org")
-		resp = self.session.get(page)
-		if resp.status_code != "200":
-			raise "Invalid Response"
-		return self.session.get(page).text
+    def __init__(self):
+        self.session = requests.Session()
 
+    def get_tpb(self, page):
+        self.session.get("https://thepiratebay.org")
+        resp = self.session.get(page)
+        #if resp.status_code != "200":
+        #    raise Exception("Invalid Response")
+        return self.session.get(page).text
+
+if __name__=='__main__':
+    # test the tpb getter
+    client = HTTPClient()
+    resp = client.get_tpb("https://thepiratebay.org/search/python/0/99/0")
+    ok = str("magnet" in resp)
+    print(ok)
+    
